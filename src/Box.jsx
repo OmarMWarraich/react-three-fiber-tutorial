de65a3900f970621dcc20b05958f25b7cf3b9c77
OmarMWarraich <earthling986@gmail.com>
2023-04-16 19:09:34 +0500

    feat: useState Hook

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,36 +1,29 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
 
 const Box = (props) => {
     const ref = useRef();
+    const [hovered, setHover] = useState(false);
+    const [rotate, setRotate] = useState(false);
     
     useFrame((_, delta) => {
-        ref.current.rotation.x += 1 * delta;
-        ref.current.rotation.y += 1 * delta;
-        ref.current.rotation.z += 2 * delta;
+        if (rotate) {
+            ref.current.rotation.x += 1 * delta
+            ref.current.rotation.y += 0.5 * delta
+        }
     })
 
     return (
         <mesh 
             {...props} 
             ref={ref}
-            onClick={(e) => console.log('click' + e.object.name)}
-            onContextMenu={(e) => console.log('context menu' + e.object.name)}
-            onDoubleClick={(e) => console.log('double click' + e.object.name)}
-            onWheel={(e) => console.log('wheel spins' + e.object.name)}
-            onPointerUp={(e) => console.log('up' + e.object.name)}
-            onPointerDown={(e) => console.log('down' + e.object.name)}
-            onPointerOver={(e) => console.log('over' + e.object.name)}
-            onPointerOut={(e) => console.log('out' + e.object.name)}
-            onPointerEnter={(e) => console.log('enter' + e.object.name)} // see note 1
-            onPointerLeave={(e) => console.log('leave' + e.object.name)} // see note 1
-            onPointerMove={(e) => console.log('move' + e.object.name)}
-            // onPointerMissed={() => console.log('missed' + e.object.name)}
-            // onUpdate={(self) => console.log('props have been updated' + e.object.name)}
-
+            scale = {hovered ? [1.1, 1.1, 1.1] : [1, 1, 1]}
+            onPointerDown={() => setRotate(!rotate)}
+            onPointerOver={() => setHover(true)}
+            onPointerOut={() => setHover(false)}
         >
             <boxGeometry args={[1, 1, 1]} />
-            <meshBasicMaterial color="hotpink" wireframe/>
+            <meshBasicMaterial color={hovered ? 0xff0000 : 0x00ff00} wireframe/>
         </mesh>
     )
 }
