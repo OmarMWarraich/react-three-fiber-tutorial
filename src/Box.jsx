f831a382f26bc802bef6f29985b13a692995b3c4
OmarMWarraich <earthling986@gmail.com>
2023-04-16 12:18:49 +0500

    feat: useRef hook

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,6 +1,11 @@
+import { useRef } from 'react';
+
 const Box = (props) => {
+    const ref = useRef();
+    console.log(ref);
+
     return (
-        <mesh {...props}>
+        <mesh {...props} ref={ref}>
             <boxGeometry args={[1, 1, 1]} />
             <meshBasicMaterial color="hotpink" wireframe/>
         </mesh>
