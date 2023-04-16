b48513051126d6fabe5c27cfae6458a25e1a15ed
OmarMWarraich <earthling986@gmail.com>
2023-04-16 21:24:17 +0500

    feat: gridHelper

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,14 @@ export default function App() {
   ]
 
   return (
-    <Canvas camera={{ position: [0, 0, 3] }}>
+    <Canvas camera={{ position: [1, 2, 3] }}>
       <Polyhedron position={[-0.75, -0.75, 0]} polyhedron={polyhedron} />
       <Polyhedron position={[0.75, -0.75, 0]} polyhedron={polyhedron} />
       <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
       <Polyhedron position={[0.75, 0.75, 0]} polyhedron={polyhedron} />
       <OrbitControls />
       <axesHelper args={[5]} />
+      <gridHelper/>
       <Stats />
       <Perf position="top-right"/>
     </Canvas>
