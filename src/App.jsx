f979f0dd332f503d08ff84b0b98269cc01d82a1b
OmarMWarraich <earthling986@gmail.com>
2023-04-21 00:04:05 +0500

    feat: Environment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Stats, OrbitControls, Circle } from '@react-three/drei'
+import { Stats, OrbitControls, Environment } from '@react-three/drei'
 import { Canvas, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
@@ -7,15 +7,12 @@ export default function App() {
 
   return (
     <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
+      <Environment files="./img/kloppenheim_07_4k.hdr" background /> 
       <directionalLight position={[3.3, 1.0, 4.4]} castShadow />
       <primitive
         object={gltf.scene}
         position={[0, 1, 0]}
-        children-0-castShadow
-      />
-      <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
-        <meshStandardMaterial />
-      </Circle>
+      />      
       <OrbitControls target={[0, 1, 0]} />
       <axesHelper args={[5]} />
       <Stats />
